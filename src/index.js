ef6bb6fd538e7a89141d7a192dcbf357bdd53a8d
Nicolas Gayraud <gayraud.nicolas12@gmail.com>
2020-12-27 20:55:43 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-// here you put all the js you want.
 
+window.onload = function() {
+
+    document.addEventListener("mousemove", parallax);
+    const bigCircle = document.querySelector(".big-circle");
+    const img = document.querySelector(".benjamin-image");
+    const littleCircle = document.querySelector(".little-circle");
+
+    function parallax(e) {
+
+        let width = window.innerWidth
+        let x = (e.clientX - (width/2));
+
+        bigCircle.style.transform = `translate3d(${x/50}px, 0, 0)`;
+        img.style.transform = `translate3d(${-x/20}px, 0, 0)`;
+        littleCircle.style.transform = `translate3d(${-x/30}px, 0, 0)`;
+    }
+
+};
